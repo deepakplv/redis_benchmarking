70a31a20b672ec25ac17ac726c11c628d3fd44b4
Deepak Kumar <deepak@plivo.com>
2017-09-01 11:15:13 +0530

    Time bound enque/deque

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 // Set the redis_host before running the script.
 // When using ElasticCache as redis_host, it should be in the same region as the publisher/consumer.
+// Note: Before running parallel enque/deque, clear the redis cache to get correct latencies.
 package main
 
 import (
@@ -10,27 +11,36 @@ import (
 	"encoding/json"
 	"sync/atomic"
 	"sync"
+	"bufio"
 )
 
 const (
-	redis_host = "localhost:6379"
+	 //redis_host = "localhost:6379"
+	redis_host = "benchmark-redis.tsfslr.ng.0001.euw2.cache.amazonaws.com:6379"
+
 	redis_service_name = "benchmark-redis"
 	redis_queue_name = "benchmark"
 	messageCount = 10000
 	prefetchLimit = 1000
-	batchSize = 1000
+	batchSize = 1000        // For batch consumer
 	enque_parallelism = 10  // No. of gorutines for enqueuing
 	deque_parallelism = 10  // No. of consumers for dequeuing
+
+	// Time bound publish/deque
+	runtimeDuration = time.Second * 60
+	publishingWorkers = 300
 )
 
 func main() {
 	taskQueue := createTaskQueue()
-	message := getMessage()
 
 	mode := os.Getenv("mode")       // Use "export mode=p" or "export mode=c"
 	if mode == "p" {
 		fmt.Println("Starting publishing")
-		BulkPublisher(taskQueue, message)
+		BulkPublisher(taskQueue)
+	} else if mode == "tbp" {
+		fmt.Println("Starting time bound publishing for duration: ", runtimeDuration)
+		TimeBoundPublisher(taskQueue, runtimeDuration)
 	} else if mode == "c" {
 		fmt.Println("Starting consuming")
 		ConsumeMessages(taskQueue)
@@ -39,13 +49,17 @@ func main() {
 		fmt.Println("Starting batch consumer")
 		BatchConsumeMessages(taskQueue)
 		select {}       // Keep batch consumer running
+	} else if mode == "tbc" {
+		fmt.Println("Starting time bound consumer for duration: ", runtimeDuration)
+		TimeBoundConsumeMessages(taskQueue)
+		select {}       // Keep batch consumer running
 	} else {
 		fmt.Println("Invalid Flag, Exiting")
 		return
 	}
 }
 
-func BulkPublisher(taskQueue rmq.Queue, message string) {
+func BulkPublisher(taskQueue rmq.Queue) {
 	var count uint64 = 1
 	var wg sync.WaitGroup
 	wg.Add(enque_parallelism)
@@ -53,12 +67,32 @@ func BulkPublisher(taskQueue rmq.Queue, message string) {
 		go func() {
 			defer wg.Done()
 			for ;count <= messageCount-enque_parallelism+1; {
+				message := getMessage()
 				publish(taskQueue, message)
 				atomic.AddUint64(&count, 1)
 			}
 		}()
 	}
 	wg.Wait()
+}
+
+func TimeBoundPublisher(taskQueue rmq.Queue, totalDuration time.Duration) {
+	var count uint64
+	for i:=0; i<publishingWorkers; i++ {
+		go func() {
+			for {
+				message := getMessage()
+				publish(taskQueue, message)
+				atomic.AddUint64(&count, 1)
+				time.Sleep(time.Millisecond*975)
+			}
+		}()
+	}
+	select {
+	        case <- time.After(totalDuration):
+	                fmt.Println(count) // This may not be correct count as goroutines are still running
+	                os.Exit(1)
+        }
 }
 
 type Message struct {
@@ -144,10 +178,9 @@ func (consumer *Consumer) Consume(delivery rmq.Delivery) {
 		fmt.Println("No. of messages dequeued per second: ", float64(1)/(timeElapsedSeconds/float64(messageCount-1)))
 		os.Exit(1)
 	}
-	if latency != 0 {
-		atomic.AddUint64(&count, 1)
-		atomic.AddUint64(&totalLatency, latency)
-	}
+
+	atomic.AddUint64(&count, 1)
+	atomic.AddUint64(&totalLatency, latency)
 
 	// Set the startTime after first message is read, so that we can get enqueues per second
 	if count == 1 {
@@ -193,10 +226,9 @@ func (consumer *BatchConsumer) Consume(batch rmq.Deliveries) {
 			fmt.Println("No. of messages dequeued per second: ", float64(1) / (timeElapsedSeconds / float64(messageCount - 1)))
 			os.Exit(1)
 		}
-		if latency != 0 {
-			atomic.AddUint64(&count, 1)
-			atomic.AddUint64(&totalLatency, latency)
-		}
+
+		atomic.AddUint64(&count, 1)
+		atomic.AddUint64(&totalLatency, latency)
 
 		// Set the startTime after first message is read, so that we can get enqueues per second
 		if count == 1 {
@@ -206,4 +238,65 @@ func (consumer *BatchConsumer) Consume(batch rmq.Deliveries) {
 
 	// Acknowledge the batch delivery
 	batch.Ack()
+}
+
+var latencyList []uint64
+var initTime time.Time
+func TimeBoundConsumeMessages(taskQueue rmq.Queue) {
+	initTime = time.Now()
+	// Start consuming and add consumers.
+	taskQueue.StartConsuming(prefetchLimit, time.Millisecond)
+	for i := 0; i < deque_parallelism; i++ {
+		name := fmt.Sprintf("TimeBoundconsumer %d", i)
+		taskQueue.AddConsumer(name, NewTimeBoundConsumer(i))
+	}
+}
+
+type TimeBoundConsumer struct {
+	name   string
+}
+
+func NewTimeBoundConsumer(tag int) *TimeBoundConsumer {
+	return &TimeBoundConsumer{
+		name:   fmt.Sprintf("TimeBoundconsumer%d", tag),
+	}
+}
+
+func (consumer *TimeBoundConsumer) Consume(delivery rmq.Delivery) {
+	// If runtime is over, write latencies to file and exit
+	if time.Since(initTime) > runtimeDuration {
+		// Create a file and write the latencies
+		file, err := os.Create("/tmp/redis_latencies.txt")
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+	        w := bufio.NewWriter(file)
+		for _, latency := range latencyList {
+	                fmt.Fprintln(w, latency)
+	        }
+		if err = w.Flush(); err != nil {
+	                panic(err)
+	        }
+                if err = w.Flush(); err != nil {
+                        panic(err)
+                }
+
+		fmt.Println("Average Latency for last item is ", totalLatency / count, count)
+		os.Exit(1)
+	}
+
+	// Dequeue the message and get the latency
+	var message Message
+	json.Unmarshal([]byte(delivery.Payload()), &message)
+	enqueue_time := message.EnqueueTime
+	latency := uint64(time.Now().UnixNano()) - enqueue_time
+
+	atomic.AddUint64(&count, 1)
+	atomic.AddUint64(&totalLatency, latency)
+	// Add latency to list after converting from nano to millisecond
+	latencyList = append(latencyList, latency/1000000)
+
+	// Acknowledge the delivery
+	delivery.Ack()
 }
